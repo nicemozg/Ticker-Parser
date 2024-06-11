1cf5d989e94ce1bebf7e761c36f3d371364e9da5
nicemozg <nice.mozg@gmail.com>
2024-06-11 22:43:24 +0500

    Add main

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Ticker-Parser
+
+go 1.22.3
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"Ticker-Parser/models"
+	"Ticker-Parser/worker"
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	configPath := "config.yaml"
+	file, err := os.Open(configPath)
+	if err != nil {
+		log.Fatalf("Error opening config file: %v", err)
+	}
+	defer file.Close()
+
+	var config models.Config
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		log.Fatalf("Error decoding config file: %v", err)
+	}
+
+	numCPU := runtime.NumCPU()
+	if config.MaxWorkers > numCPU {
+		config.MaxWorkers = numCPU
+	}
+
+	workers := make([]*worker.Worker, config.MaxWorkers)
+	for i := 0; i < config.MaxWorkers; i++ {
+		workers[i] = &worker.Worker{}
+	}
+
+	for i, symbol := range config.Symbols {
+		workers[i%config.MaxWorkers].Symbols = append(workers[i%config.MaxWorkers].Symbols, symbol)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+
+	for _, wrk := range workers {
+		wg.Add(1)
+		go wrk.Run(ctx, &wg)
+	}
+
+	ticker := time.NewTicker(5 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				totalRequests := 0
+				for _, wrk := range workers {
+					totalRequests += wrk.GetRequestsCount()
+				}
+				fmt.Printf("workers requests total: %d\n", totalRequests)
+			}
+		}
+	}()
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-stopChan
+		fmt.Println("Received stop signal, shutting down...")
+		cancel()
+	}()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Text() == "STOP" {
+			cancel()
+			break
+		}
+	}
+
+	wg.Wait()
+}
